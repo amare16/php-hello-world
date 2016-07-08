dd61637314abe1f56abb03d8dbc17bdfc598cc62
amare <amare>
2016-07-08 20:11:15 +0200

    amare's first commit on php hello world

--- hello-world.php
+++ hello-world.php
@@ -1,13 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <p><?php
 
-//
-// This is only a SKELETON file for the "Hello World" exercise.
-// It's been provided as a convenience to get you started writing code faster.
-//
+            //
+            // This is only a SKELETON file for the "Hello World" exercise.
+            // It's been provided as a convenience to get you started writing code faster.
+            //
 
-function helloWorld($name)
-{
-    //
-    // YOUR CODE GOES HERE
-    //
-}
+            function helloWorld($name)
+            {
+                //
+                // YOUR CODE GOES HERE
+                //
+                echo "<strong>Hello World!</strong>";
+            }
+            helloWorld($name);
+            ?>
+        </p>
+        <img src="Gif_image.gif" /><br />
+        <p><strong>Just Enjoy with this music. Live only today, Don't care about tomorrow!!!</strong></p><br />
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/al2DFQEZl4M" frameborder="0" allowfullscreen>
+        </iframe>
+    </body>
+</html>
