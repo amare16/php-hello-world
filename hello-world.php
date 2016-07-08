6a3f4fc570848edccbe6e5c55e820aa283a07c7e
amare <amare>
2016-07-08 21:37:34 +0200

    amare's second commit on php-hello-world

--- hello-world.php
+++ hello-world.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Hello World</title>
+        <link type="text/css" rel="stylesheet" href="style.css" />
     </head>
     <body>
         <p><?php
@@ -18,12 +19,24 @@
                 //
                 echo "<strong>Hello World!</strong>";
             }
-            helloWorld($name);
+            helloWorld();
             ?>
         </p>
         <img src="Gif_image.gif" /><br />
-        <p><strong>Just Enjoy with this music. Live only today, Don't care about tomorrow!!!</strong></p><br />
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/al2DFQEZl4M" frameborder="0" allowfullscreen>
-        </iframe>
+        <p><strong>Live only today, Don't care about tomorrow!!!</strong></p><br />
+        <p>You have a surprise below, please click it quickly</p>
+        <button id="demo" onclick="myFunction()">Click me</button>
+        <p id="demo"></p>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script>
+            function myFunction() {
+                var obj = {"video": {
+  "value": "<iframe title='YouTube video player' type=\"text/html\" width='640' height='390' src='http://www.youtube.com/embed/W-Q7RMpINVo' frameborder='0' allowFullScreen></iframe>"
+}}
+
+$("#demo").html(obj.video.value);
+            }
+        </script>
+
     </body>
 </html>
