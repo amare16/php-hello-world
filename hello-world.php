89d020f91c5679eb7092d34d2aed2d6a32dcc028
amare <amare>
2016-07-13 17:28:12 +0200

    amare's updated php hello world

--- hello-world.php
+++ hello-world.php
@@ -3,7 +3,7 @@
             // This is only a SKELETON file for the "Hello World" exercise.
             // It's been provided as a convenience to get you started writing code faster.
             //
-function helloWorld($name) {
+function helloWorld($name = "") {
             if ($name == "") {
                         return "string"; "Helll, World!";
             } else {
